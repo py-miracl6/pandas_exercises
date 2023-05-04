dd3cc5c803e2dce757c309b85be07079719c7a79
miracl6 <annikulina.08@gmail.com>
2023-05-04 23:32:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title: Pandas Exercises New
+emoji: 📚
+colorFrom: indigo
+colorTo: blue
+sdk: streamlit
+sdk_version: 1.19.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ pandas_func.py
@@ -0,0 +1,29 @@
+import sys
+import streamlit as st
+from io import StringIO
+import contextlib
+
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+
+def hide_part_of_page():
+    st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+    hide_streamlit_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        [data-testid="collapsedControl"] {
+        display: none
+        }
+        [kind="header"] {visibility: hidden;}
+        [data-testid="stHeader"] {visibility: hidden;}
+        </style>"""
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
